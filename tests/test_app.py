58dbfea6d688970753af354ae2154d74655d8e4e
ht-yarll <h.scott.xp@gmail.com>
2025-01-27 20:31:15 -0300

    :tada: init: project start

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_read_root_should_return_ok_e_hellouworld():
+    client = TestClient(app)  # Arrange
+
+    response = client.get('/')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Hellou World🌎'}
