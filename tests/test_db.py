c9433db0ab23ec4f15d463a7c9d1070fcdcfa727
ht-yarll <h.scott.xp@gmail.com>
2025-02-01 15:33:06 -0300

    :test_tube: test success for updated_at

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,12 +1,22 @@
+from dataclasses import asdict
+
 from sqlalchemy import select
 
 from fast_zero.models import User
 
 
-def test_create_user(session):
-    user = User(username='yarll', email='yar@ll.com', password='password')
-    session.add(user)
-    session.commit()
-    result = session.scalar(select(User).where(User.email == 'yar@ll.com'))
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(username='yarll', email='yar@ll.com', password='password')
+        session.add(new_user)
+        session.commit()
+        user = session.scalar(select(User).where(User.username == 'yarll'))
 
-    assert result.username == 'yarll'
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'yarll',
+        'password': 'password',
+        'email': 'yar@ll.com',
+        'created_at': time,
+        'updated_at': time
+    }
